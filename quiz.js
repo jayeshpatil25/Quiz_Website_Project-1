51cf75b6ababd95464daacb6e3ef6bd7aa6c2271
jayeshpatil25 <jayeshpatil7170@gmail.com>
2024-06-03 22:45:16 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,684 @@
+let questions1 = [
+    {
+        numb: 1,
+        question: "Which is the smallest prime number?",
+        answer: "2",
+        options: [
+            "3",
+            "2",
+            "5",
+            "4"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the value of cos 0?",
+        answer: "1",
+        options: [
+            "0",
+            "1/2",
+            "(root 3)/2",
+            "1"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the cube root of 64?",
+        answer: "4",
+        options: [
+            "16",
+            "8",
+            "4",
+            "2"
+        ]
+    }
+]
+
+let questions2 = [
+    {       numb: 1,
+                question: "If three times a number added to 5 equals 32, what is the number?",
+                answer: "9",
+                options: [
+                    "6",
+                    "7",
+                    "8",
+                    "9"
+                ]
+            },
+            {
+                numb: 2,
+                question: "The score of Emma was half as that of Ava and the score of Mia was twice that of Ava. If the score of Mia was 40, what is the score, Emma?",
+                answer: "10",
+                options: [
+                    "10",
+                    "18",
+                    "20",
+                    "24"
+                ]
+            },
+            {
+                numb: 3,
+                question: "Two-third of 9 is equal to 2/5 of which number?",
+                answer: "15",
+                options: [
+                    "20",
+                    "15",
+                    "30",
+                    "25"
+                ]
+            }
+]
+
+let questions3 = [
+    {
+        numb: 1,
+        question: "When a number is subtracted from 32 and the difference is divided by that number, the result is 3. What is the value of the number?",
+        answer: "8",
+        options: [
+            "4",
+            "2",
+            "8",
+            "6"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Solve for x :4(x+1)=6(x-4)+20?",
+        answer: "4",
+        options: [
+            "8",
+            "4",
+            "2",
+            "1"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Two-kilogram apple and two-kilogram orange cost $26.4. If a one-kilogram apple costs $4.2 how much does one-kilogram orange cost?",
+        answer: "$9",
+        options: [
+            "$9",
+            "$5.5",
+            "$4.5",
+            "$6"
+        ]
+    }
+]
+
+let questions4 = [
+    {
+        numb: 1,
+        question: "What is the chemical symbol for the element gold?",
+        answer: "Au",
+        options: [
+            "Sn",
+            "Ag",
+            "Au",
+            "Cu"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following is an example of a renewable energy source?",
+        answer: "Solar Power",
+        options: [
+            "Coal",
+            "Natural Gas",
+            "Nuclear Energy",
+            "Solar Power"
+        ]
+    },
+    {
+        numb: 3,
+        question: " What is the SI unit of electric current?",
+        answer: "Ampere",
+        options: [
+            "Watt",
+            "Ampere",
+            "Ohm",
+            "Volt"
+        ]
+    }
+]
+
+let questions5 = [
+    {
+        numb: 1,
+        question: " What is the name of juice secreted from the gall bladder?",
+        answer: "Bile juice",
+        options: [
+            "Saliva",
+            "Hydrochloric acid",
+            "Bile juice",
+            "Maltase"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following is an alkaline earth metal?",
+        answer: "Calcium",
+        options: [
+            "Potassium",
+            "Sodium",
+            "Aluminium",
+            "Calcium"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which device is used for measuring air pressure?",
+        answer: "Barometer",
+        options: [
+            "Ammeter",
+            "Seismograph",
+            "Barometer",
+            "Calorimeter"
+        ]
+    }
+]
+
+let questions6 = [
+    {
+        numb: 1,
+        question: "What is the main function of the enzyme amylase in the digestive system?",
+        answer: "Break down carbohydrates",
+        options: [
+            "Break down carbohydrates",
+            "Break down proteins",
+            "Break down fats",
+            "Aid in bile production"
+        ]
+    },
+    {
+        numb: 2,
+        question: "A passenger in a moving bus is thrown forward when the bus suddenly stops. This is explained by:",
+        answer: "Newton's First Law of Motion",
+        options: [
+            "Newton's First Law of Motion",
+            "Newton's Second Law of Motion",
+            "Newton's Third Law of Motion",
+            "Law of conservation of Momentum"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the following animal breathes from skin?",
+        answer: "Earthworm",
+        options: [
+            "Snake",
+            "Earthworm",
+            "Fish",
+            "Monkey"
+        ]
+    }
+]
+
+let questions7 = [
+    {
+        numb: 1,
+        question: "Choose the correct sentence:",
+        answer: "He plays soccer on Saturdays.",
+        options: [
+            "They goes to the beach every weekend.",
+            "She don't like ice cream.",
+            "He plays soccer on Saturdays.",
+            "We is going to the party tonight."
+        ]
+    },
+    {
+        numb: 2,
+        question: "Select the grammatically correct sentence:",
+        answer: "I am going to the store to buy some groceries.",
+        options: [
+            "The dog wag his tail happily.",
+            "She run fast in the race.",
+            "They were go to the movie theater yesterday.",
+            "I am going to the store to buy some groceries."
+        ]
+    },
+    {
+        numb: 3,
+        question: "Choose the correct sentence:",
+        answer: "She goes to school by bus.",
+        options: [
+            "He have a lot of friends.",
+            "She goes to school by bus.",
+            "They is playing football now.",
+            "We don't likes rainy days."
+        ]
+    }
+]
+
+let questions8 = [
+    {
+        numb: 1,
+        question: "Choose the sentence with the correct use of the verb tense:",
+        answer: "She will go to the concert tomorrow.",
+        options: [
+            "She will going to the concert tomorrow.",
+            "She will go to the concert tomorrow.",
+            "She going to go to the concert tomorrow.",
+            "She went to the concert tomorrow."
+        ]
+    },
+    {
+        numb: 2,
+        question: "Rewrite the following in passive voice: The chef cooked a delicious meal.",
+        answer: "A delicious meal was cooked by the chef.",
+        options: [
+            "A delicious meal cooks the chef.",
+            "A delicious meal is cooked by the chef.",
+            "A delicious meal was cooked by the chef.",
+            "A delicious meal had cooked by the chef."
+        ]
+    },
+    {
+        numb: 3,
+        question: "Identify the correct sentence:",
+        answer: "The cat and the dog are playing in the garden.",
+        options: [
+            "The cat and the dog is playing in the garden.",
+            "The cat and the dog are playing in the garden.",
+            "The cat and the dog was playing in the garden",
+            "The cat and the dog be playing in the garden."
+        ]
+    }
+]
+
+let questions9 = [
+    {
+        numb: 1,
+        question: "Identify the correct sentence:",
+        answer: "Each of the students are studying for their exams.",
+        options: [
+            "Each of the students are studying for their exams.",
+            "Every student is studying for their exams..",
+            "Each student is studying for their exams.",
+            "The students is studying for their exams."
+        ]
+    },
+    {
+        numb: 2,
+        question: "Identify type f sentence: When you finish your homework, you can watch TV.",
+        answer: "Complex sentence.",
+        options: [
+            "Simple sentence.",
+            "Compound sentence.",
+            "Complex sentence.",
+            "Compound-complex sentence."
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which sentence is grammatically correct?",
+        answer: "Neither John nor his friends is coming to the party.",
+        options: [
+            "Neither John nor his friends is coming to the party.",
+            "Neither John nor his friends are coming to the party.",
+            "Neither John or his friends is coming to the party.",
+            "Neither John nor his friends are coming to the party."
+        ]
+    }
+]
+
+let submit1 = document.querySelector("#submit1");
+let submit2 = document.querySelector("#submit2");
+let submit3 = document.querySelector("#submit3");
+let submit4 = document.querySelector("#easy");
+let submit5 = document.querySelector("#medium");
+let submit6 = document.querySelector("#hard");
+
+if (submit1) {
+    submit1.onclick = () => {
+        let search = document.querySelector("#s1").value.trim().toLowerCase();
+        if (search) {
+            if (search == "maths" || search == "english" || search == "science") {
+                selectedCategory = search;
+                localStorage.setItem('selectedCategory', selectedCategory);
+                window.location.href = "Difficulty.html";
+            } else {
+                alert("Please enter an appropriate quiz category!");
+            }
+        } else {
+            alert("Please enter a quiz category!");
+        }
+        document.getElementById("s1").value = "";
+        document.getElementById("voiceinput").value = "";
+    };
+}
+
+if (submit2) {
+    submit2.onclick = () => {
+        let search = document.querySelector("#s2").value.trim().toLowerCase();
+        if (search) {
+            if (search == "maths" || search == "english" || search == "science") {
+                selectedCategory = search;
+                localStorage.setItem('selectedCategory', selectedCategory);
+                window.location.href = "Difficulty.html";
+            } else {
+                alert("Please select a quiz category!");
+            }
+        } else {
+            alert("Please select a quiz category!");
+        }
+        document.getElementById("s1").value = "";
+        document.getElementById("voiceinput").value = "";
+    };
+}
+
+if (submit3) {
+    submit3.onclick = () => {
+        let search = document.querySelector("#voiceinput").value.trim().toLowerCase();
+        if (search) {
+            if (search == "maths" || search == "english" || search == "science") {
+                selectedCategory = search;
+                localStorage.setItem('selectedCategory', selectedCategory);
+                window.location.href = "Difficulty.html";
+            } else {
+                alert("Please speak out an appropriate quiz category!");
+            }
+        } else {
+            alert("Please speak out a quiz category!");
+        }
+        document.getElementById("s1").value = "";
+        document.getElementById("voiceinput").value = "";
+    };
+}
+
+window.speak = function() {
+    let recognition = new webkitSpeechRecognition();
+    recognition.lang = "en-GB";
+    recognition.onresult = function(event) {
+        console.log(event);
+        document.getElementById("voiceinput").value = event.results[0][0].transcript;
+    };
+    recognition.start();
+};
+
+if(submit4)
+{
+    submit4.onclick = () =>
+        {
+            selectedDifficulty = document.querySelector("#easy").value.trim().toLowerCase();
+            localStorage.setItem('selectedDifficulty', selectedDifficulty);
+            window.location.href = "quiz.html";
+        }
+}
+if(submit5)
+{
+    submit5.onclick = () =>
+        {
+            selectedDifficulty = document.querySelector("#medium").value.trim().toLowerCase();
+            localStorage.setItem('selectedDifficulty', selectedDifficulty);
+            window.location.href = "quiz.html";
+        }
+}
+if(submit6)
+    {
+        submit6.onclick = () =>
+            {
+                selectedDifficulty = document.querySelector("#hard").value.trim().toLowerCase();
+                localStorage.setItem('selectedDifficulty', selectedDifficulty);
+                window.location.href = "quiz.html";
+            }
+    }
+
+let selectedCategory = localStorage.getItem('selectedCategory');
+let selectedDifficulty = localStorage.getItem('selectedDifficulty');
+
+let questions = [];
+if(selectedCategory == "maths" && selectedDifficulty == "easy")
+    {
+        questions = questions1;
+    }
+        
+if(selectedCategory == "maths" && selectedDifficulty == "medium")
+    {
+        questions = questions2;
+    }
+        
+if(selectedCategory == "maths" && selectedDifficulty == "hard")
+    {
+        questions = questions3;
+    }
+        
+if(selectedCategory == "science" && selectedDifficulty == "easy")
+    {
+        questions = questions4;
+    }
+        
+if(selectedCategory == "science" && selectedDifficulty == "medium")
+    {
+        questions = questions5;
+    }
+        
+if(selectedCategory == "science" && selectedDifficulty == "hard")
+    {
+        questions = questions6;
+    }
+        
+if(selectedCategory == "english" && selectedDifficulty == "easy")
+    {
+        questions = questions7;
+    }
+        
+if(selectedCategory == "english" && selectedDifficulty == "medium")
+    {
+        questions = questions8;
+    }
+        
+if(selectedCategory == "english" && selectedDifficulty == "hard")
+    {
+        questions = questions9;
+    }
+
+const start_btn = document.querySelector(".start_btn");
+const info_box = document.querySelector(".info_box");
+const exit_btn = document.querySelector(".info_box .buttons .quit");
+const continue_btn = document.querySelector(".info_box .buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const timeCount = document.querySelector(".quiz_box .timer_sec");
+const timeOff = document.querySelector(".quiz_box header .time_text");
+
+const option_list = document.querySelector(".option_list");
+
+if(start_btn)
+{
+start_btn.onclick = () =>
+{
+    info_box.classList.add("activeInfo");
+}
+}
+
+if(exit_btn)
+{
+exit_btn.onclick = () =>
+{
+    window.location.href = "home.html";
+}    
+}
+
+if(continue_btn)
+{
+continue_btn.onclick = () =>
+{
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
+    showQuestions(0);
+    queCounter(1);
+    startTimer(30);
+}
+}
+
+let que_count = 0;
+let que_numb = 1;
+let counter;
+let timeValue = 30;
+let userScore = 0;
+
+const next_btn = document.querySelector(".quiz_box footer .next_btn");
+const result_box = document.querySelector(".result_box");
+const restart_quiz = document.querySelector(".result_box .buttons .restart");
+const quit_quiz = document.querySelector(".result_box .buttons .quit");
+
+if(restart_quiz)
+{
+restart_quiz.onclick = () =>
+{
+    result_box.classList.remove("activeResult");
+    info_box.classList.add("activeInfo");
+    let que_count = 0;
+    let que_numb = 1;
+    let timeValue = 30;
+    let userScore = 0;
+    showQuestions(que_count);
+    queCounter(que_numb);
+    clearInterval(counter);
+    startTimer(timeValue);
+    next_btn.style.display = "none";
+    timeOff.textContent = "Time Left";
+    window.location.href = "quiz.html";
+}
+}
+
+if(quit_quiz)
+{
+quit_quiz.onclick = () =>
+{
+    window.location.href = "home.html";
+}
+}
+
+if(next_btn)
+{
+next_btn.onclick = () =>
+{
+    if(que_count < questions.length - 1)
+    {
+        que_count++;
+        que_numb++;
+        showQuestions(que_count);
+        queCounter(que_numb);
+        clearInterval(counter);
+        startTimer(timeValue);
+        next_btn.style.display = "none";
+        timeOff.textContent = "Time Left";
+    } 
+    else
+    {
+        clearInterval(counter);
+        console.log("Questions Completed");
+        showResultBox();
+    }
+}
+}
+
+function showQuestions(index)
+{
+const que_text = document.querySelector(".que_text");
+let que_tag = '<span>'+ questions[index].numb + "." + questions[index].question +'</span>';
+let option_tag = '<div class = "option"><span>'+ questions[index].options[0] +'</span></div>'
+                 + '<div class = "option"><span>'+ questions[index].options[1] +'</span></div>'
+                 + '<div class = "option"><span>'+ questions[index].options[2] +'</span></div>'
+                 + '<div class = "option"><span>'+ questions[index].options[3] +'</span></div>';
+que_text.innerHTML = que_tag;
+option_list.innerHTML = option_tag;
+
+const option = option_list.querySelectorAll(".option");
+for(let i = 0; i < option.length; i++)
+    {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+function optionSelected(answer)
+{
+clearInterval(counter);
+let userAns = answer.textContent;
+let correctAns = questions[que_count].answer;
+let allOptions =  option_list.children.length;
+if(userAns == correctAns)
+    {
+        userScore += 1;
+        console.log(userScore);
+        answer.classList.add("correct");
+        console.log("Answer is Correct");
+    }
+else
+{
+    answer.classList.add("incorrect");
+    console.log("Answer is Wrong");
+
+    for(let i = 0; i < allOptions; i++)
+    {
+       if(option_list.children[i].textContent == correctAns)
+        {
+            option_list.children[i].setAttribute("class", "option correct")
+        }
+    }
+}
+
+for(let i = 0; i < allOptions; i++)
+    {
+        option_list.children[i].classList.add("disabled");
+    }
+next_btn.style.display = "block";
+}
+
+function showResultBox()
+{
+info_box.classList.remove("activeInfo");
+quiz_box.classList.remove("activeQuiz");
+result_box.classList.add("activeResult");
+const scoreText = result_box.querySelector(".score_text");
+if(userScore >= 1)
+    {
+        let scoreTag = '<span>Well Done! You got '+ userScore + ' out of '+ questions.length + ' questions correct. </span>'
+        scoreText.innerHTML = scoreTag;
+    }
+else
+{
+    let scoreTag = '<span> Oops! You got '+ userScore + ' out of '+ questions.length + ' questions correct. </span>'
+    scoreText.innerHTML = scoreTag;
+}
+}
+
+function startTimer(time)
+{
+counter = setInterval(timer, 1000);
+function timer()
+{
+    timeCount.textContent = time;
+    time--;
+    if(time < 0)
+    {
+        clearInterval(counter);
+        timeCount.textContent = "0";
+        timeOff.textContent = "Time Over";
+
+        let correctAns = questions[que_count].answer;
+        let allOptions =  option_list.children.length;
+    
+        for(let i = 0; i < allOptions; i++)
+            {
+               if(option_list.children[i].textContent == correctAns)
+                {
+                    option_list.children[i].setAttribute("class", "option correct")
+                }
+            }
+        for(let i = 0; i < allOptions; i++)
+            {
+                option_list.children[i].classList.add("disabled");
+            }
+        next_btn.style.display = "block";
+        
+    }
+}
+}
+
+function queCounter(que_numb)
+{
+const bottom_ques_counter = quiz_box.querySelector(".total_que");
+let totalQuesCountTag = '<span>'+ que_numb + ' of ' + questions.length + ' Questions' + '</span>';
+bottom_ques_counter.innerHTML = totalQuesCountTag;
+}
